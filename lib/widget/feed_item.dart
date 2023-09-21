bf3d5267ac90cfcba700cf4dd1a581cb9eb4abbc
Emmanuel Uchenna <Camplodgeinc@gmail.com>
2023-09-21 07:42:53 -0700

    debugging null error

--- lib/widget/feed_item.dart
+++ lib/widget/feed_item.dart
@@ -1,7 +1,12 @@
 import 'package:bonjour/utils/colors.dart';
+import 'package:bonjour/widget/like_animation.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
+import '../models/user.dart';
+import '../providers/user_provider.dart';
 import '../views/appscreens/widgetscrren/comment.dart';
 
 class FeedItem extends StatefulWidget {
@@ -34,7 +39,7 @@ class FeedItem extends StatefulWidget {
 }
 
 class _FeedItemState extends State<FeedItem> {
-  bool isliked = false;
+  bool islikedAnimating = false;
 
   @override
   void initState() {
@@ -44,53 +49,85 @@ class _FeedItemState extends State<FeedItem> {
 
   @override
   Widget build(BuildContext context) {
+    User user = Provider.of<UserProvider>(context).getUser;
     return Padding(
       padding: const EdgeInsets.all(0.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            height: MediaQuery.of(context).size.height * 0.35,
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 6),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              // border: Border.all(color: Colors.white),
-              color: mobileBackgroundColor,
-              // color: Colors.white,
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage(widget.snap['postUrl']),
-              ),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
+          GestureDetector(
+            onDoubleTap: () {
+              setState(() {
+                islikedAnimating = !islikedAnimating;
+              });
+            },
+            child: Stack(
+              alignment: Alignment.center,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    //image
-                    CircleAvatar(
-                      backgroundImage:
-                          NetworkImage(widget.snap['profileImage']),
-                      radius: 20,
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.35,
+                  width: double.infinity,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 6),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    // border: Border.all(color: Colors.white),
+                    color: mobileBackgroundColor,
+                    // color: Colors.white,
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(widget.snap['postUrl']),
                     ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          //image
+                          CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(widget.snap['profileImage']),
+                            radius: 20,
+                          ),
 
-                    //username
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8),
-                      child: Text(widget.snap['username']),
-                    ),
-                  ],
+                          //username
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8),
+                            child: Text(widget.snap['username']),
+                          ),
+                        ],
+                      ),
+                      //menu
+                      IconButton(
+                          onPressed: widget.menuAction,
+                          icon: const Icon(Icons.more_vert))
+                    ],
+                  ),
                 ),
-                //menu
-                IconButton(
-                    onPressed: widget.menuAction,
-                    icon: const Icon(Icons.more_vert))
+
+                //Like widget
+                LikeAnimation(
+                  isAnimating: islikedAnimating,
+                  duration: const Duration(milliseconds: 400),
+                  onEnd: () {
+                    setState(() {
+                      islikedAnimating = !islikedAnimating;
+                    });
+                  },
+                  child: const Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                    size: 100,
+                  ),
+                )
               ],
             ),
           ),
+
+          ///////////
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -99,20 +136,14 @@ class _FeedItemState extends State<FeedItem> {
                 children: [
                   //favourite
 
-                  IconButton(
-                      onPressed: () {
-                        setState(() {
-                          isliked = !isliked;
-                        });
-                      },
-                      icon: isliked
-                          ? const Icon(
-                              Icons.favorite,
-                              color: Colors.red,
-                            )
-                          : const Icon(
-                              Icons.favorite,
-                            )),
+                  LikeAnimation(
+                    isAnimating: widget.snap['likes'].contains(user.uid),
+                    smallLike: true,
+                    child: IconButton(
+                      icon: const Icon(Icons.favorite),
+                      onPressed: () {},
+                    ),
+                  ),
 
                   //comment
                   IconButton(
@@ -166,7 +197,7 @@ class _FeedItemState extends State<FeedItem> {
                     child: const Text("View all comment..")),
                 //Date
                 Container(
-                  padding: EdgeInsets.symmetric(),
+                  padding: const EdgeInsets.symmetric(),
                   child: Text(
                       //Getting date format from the db
                       DateFormat.yMMMd()
--- lib/widget/like_animation.dart
+++ lib/widget/like_animation.dart
@@ -1,47 +1,67 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class LikeAnimation extends StatefulWidget {
-//   const LikeAnimation(
-//       {super.key,
-//       required this.child,
-//       required this.isAnimating,
-//       required this.duration,
-//       this.onEnd});
-//   final Widget child;
-//   final bool isAnimating;
-//   final Duration duration = const Duration(milliseconds: 150);
-//   final bool smallLike = false;
-//   final VoidCallback? onEnd;
+class LikeAnimation extends StatefulWidget {
+  const LikeAnimation(
+      {super.key,
+      required this.child,
+      required this.isAnimating,
+      this.duration = const Duration(milliseconds: 150),
+      this.onEnd,
+      this.smallLike = false});
+  final Widget child;
+  final bool isAnimating;
+  final Duration duration;
+  final bool smallLike;
+  final VoidCallback? onEnd;
 
-//   @override
-//   State<LikeAnimation> createState() => _LikeAnimationState();
-// }
+  @override
+  State<LikeAnimation> createState() => _LikeAnimationState();
+}
 
-// class _LikeAnimationState extends State<LikeAnimation>
-//     with SingleTickerProviderStateMixin {
-//   late AnimationController controller;
+class _LikeAnimationState extends State<LikeAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> scale;
 
-//   @override
-//   void initState() {
-//     controller = AnimationController(
-//       vsync: this,
-//       duration: Duration(milliseconds: widget.duration.inMilliseconds ~/ 2),
-//     );
-//     scale = Tween<double>(begin: 1, end: 1.2).animate(controller);
-//     super.initState();
-//   }
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: widget.duration.inMilliseconds ~/ 2),
+    );
+    scale = Tween<double>(begin: 1, end: 1.2).animate(controller);
+    super.initState();
+  }
 
-//   @override
-//   void didUpdateWidget(covariant LikeAnimation oldWidget) {
-//     super.didUpdateWidget(oldWidget);
+  @override
+  void didUpdateWidget(covariant LikeAnimation oldWidget) {
+    super.didUpdateWidget(oldWidget);
 
-//     if (widget.isAnimating != oldWidget.isAnimating) {
-//       await co
-//     } else {}
-//   }
+    if (widget.isAnimating != oldWidget.isAnimating) {
+      startAnimation();
+    }
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container();
-//   }
-// }
+  startAnimation() async {
+    if (widget.isAnimating || widget.smallLike) {
+      await controller.forward();
+      await controller.reverse();
+      await Future.delayed(const Duration(milliseconds: 200));
+
+      if (widget.onEnd != null) {
+        widget.onEnd;
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(scale: scale, child: Container());
+  }
+}
